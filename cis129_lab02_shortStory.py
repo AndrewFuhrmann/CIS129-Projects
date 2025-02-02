072fc13c96d7e618b40ff14539f9db765a88a653
AndrewFuhrmann <afuhrmann@mail.pima.edu>
2025-02-01 18:09:30 -0700

    Updated cis129_lab02_shortStory.py
    Deleted the original print code and added 3 comment lines as well as 4 new print code lines

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,2 +1,9 @@
-print('Hello, World!')
+# Author: Andrew Fuhrmann 
+# My first Python lab in my CIS129 course
+# Prints a simple short poem
+print('His chest was tight, his hands felt cold,')
+print('A quiet fear that took its hold.')
+print('He breathed in deep, let stillness grow,')
+print('And felt his worries come and go.')
+
 
